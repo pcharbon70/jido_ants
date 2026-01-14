5b8590b38d4e522228f73077af954f94d4479663
Pascal Charbonneau <pcharbon70@gmail.com>
2026-01-14 13:32:50 -0500

    Add Phoenix.PubSub to Application (Phase 1.2.1)
    Implement section 1.2.1 of Phase 1.2 PubSub Configuration:
    Add Phoenix.PubSub to Application supervision tree.
    
    - Create AntColony.Events module with topic constants
    - Define type specifications for all simulation events
    - Add Phoenix.PubSub to supervision tree as AntColony.PubSub
    - Update Application module with proper documentation

--- lib/ant_colony/application.ex
+++ lib/ant_colony/application.ex
@@ -1,19 +1,19 @@
 defmodule AntColony.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
+  @moduledoc """
+  Application module for the ant_colony OTP application.
+
+  Configures and starts the supervision tree for the simulation.
+  """
 
   use Application
 
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: AntColony.Worker.start_link(arg)
-      # {AntColony.Worker, arg}
+      # Phoenix.PubSub for event-driven communication
+      {Phoenix.PubSub, name: AntColony.PubSub}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: AntColony.Supervisor]
     Supervisor.start_link(children, opts)
   end
